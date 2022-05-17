0b11f26b24d98e72bcd9e229c26d99fcfe732add
Wolf Pinz <wolfpinz@gmail.com>
2022-05-17 16:35:09 +0200

    challenge done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action
   # 7 CRUD ACTIONS
   # index, show, new, create, edit, update, destroy
   def index
@@ -9,20 +10,37 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
-  # def new
-  # end
+  def new
+    @task = Task.new
+  end
 
-  # def create
-  # end
+  def create
+    @task = Task.create(task_params)
+    redirect_to tasks_path(@task)
+  end
 
-  # def edit
-  # end
+  def edit
+    @task = Task.find(params[:id])
+  end
 
-  # def update
-  # end
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
 
-  # def destroy
-  # end
+    redirect_to task_path(@task)
+  end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,15 @@ Rails.application.routes.draw do
   # 7 CRUD ACTIONS
   # index, all, new, create, edit, update, destroy
   # I.1 READ all
-  get '/', to: 'tasks#index'
+  get '/tasks', to: 'tasks#index'
   # # II. CREATE - 1. get, 2. post
-  # get '/tasks/new', to: 'tasks#new'
-  # post '/tasks', to: 'tasks#create'
+  get '/tasks/new', to: 'tasks#new'
+  post '/tasks', to: 'tasks#create', as: 'task_create'
   # I.2 READ specific
-  get '/tasks/:id', to: 'tasks#show'
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
   # # III. UPDATE
-  # get '/tasks/:id/edit', to: 'tasks#edit'
-  # patch '/tasks/:id', to: 'tasks#update'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'task_edit'
+  patch '/tasks/:id', to: 'tasks#update', as: 'task_update'
   # # IV. DELETE
-  # delete '/restaurants/:id', to: 'restaurants#destroy'
+  delete '/tasks/:id', to: 'tasks#destroy', as: 'task_destroy'
 end
